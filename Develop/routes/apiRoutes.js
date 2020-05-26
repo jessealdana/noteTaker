a7ea72e39e19880ac9e38517727530782a5629df
Jesse Aldana <jesse.aldana@gmail.com>
2020-05-26 16:08:41 -0700

    edited README

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,9 @@
 var path = require("path");
 const dataPath = path.join(__dirname, "../db/db.json");
-activeNote = require("../db/db.json");
+const newNote = require("../public/assets/js/index.js");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const apiRoutes = (app,fs) => {
 
@@ -10,11 +12,11 @@ const apiRoutes = (app,fs) => {
             if (err) {
                 throw err;
             }
-            callback(returnJson ? JSON.parse(activeNote) : activeNoteData);
+            callback(returnJson ? JSON.parse(data) : data);
         });
     };
 
-    const writeFile = (fileData, callback, filePath = dataPath, encoding = "utf8") => {
+    const writeFile = (fileData = newNote, callback, filePath = dataPath, encoding = "utf8") => {
         fs.writeFile(filePath, fileData, encoding, (err) => {
             if (err) {
                 throw err;
@@ -24,31 +26,27 @@ const apiRoutes = (app,fs) => {
     };
     
     app.get("/api/notes", (req, res) => {
-        readFile( (activeNote) => {
-            res.json(true);
+            res.json(newNote);
         });   
+ 
+    app.post("/api/notes", (req, res) => {
+           var newNote = req.body;
+           readFile(newNote, (req, res) => {
+                writeFile(newNote, (req, res) => {
+                    activeNote.push(newNote.JSONstringify())
+                    res.json(newNote);
+            });
+        });
     });
 
-    app.post("/api/notes", (req, res) => {
-        readFile( (activeNote) => {
-            res.json(activeNote);
-        writeFile(JSON.stringify(activeNote), () => {
-                activeNote + id.push(req.body);
-                res.json(true);
+    app.delete("api/notes/" + id, (req, res) => {       
+        fs.unlink( (activeNote) => {
+
+            console.log("complete")
         });
     },
         true);
-    });
 
-
-//     app.delete("api/notes/" + id, (req, res) => {
-//         fs.unlink( (activeNote) => {
-//         writeFile(JSON.stringify(activeNote), () => {
-//             console.log("complete")
-//         });
-//     },
-//         true);
-//     });
 
  };
 module.exports = apiRoutes;
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# noteTaker
+
+
+# NoteTaker
+  ![Mozilla Add-on](https://img.shields.io/amo/v/blue?color=blue&label=employee%20Rosterizer&logoColor=white)
+  
+ ## Description
+ 
+ This app is meant to keep a list of saved notes which the user can edit and delete as needed.  I found this homework to be an overwhelming challenge and I did not create a functional version.  I have no excuse. I actually spent two sessions with a learning assistant on this project and at the beginning I felt like I'd had relevant examples to reference in the class activities.  However, as I started I realized I had no clear direction and I began to google a general approach.  I found something I felt was close to what I needed (the link is in the credits section below) and thus started down a rabbit hole which it took the learning assistant that I work with some time from which to extricate me.  In the process I learned about keeping separate files for the front and back end as well as the importance of where those files are kept and what can or should be exported from them.  But I did not finish a working NoteTaker.
+ 
+ 
+
+ link to github repo:https://jessealdana.github.io/employeeRoster/
+ 
+ ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Questions](#questions)
+ ## Installation
+
+ This app is run from the command line and can be seen on localhost:8080.
+
+ ## Usage
+ 
+ The user is meant to enter a note and title in the appropriate text areas in the browser window.  Currently, text may be entered but does not save.
+
+ ## License
+ 
+ This App is licensed under the MIT license.
+
+ ## Credits
+ 
+ Jesse Aldana. Rob Kendal https://robkendal.co.uk/blog/how-to-build-a-restful-node-js-api-server-using-json-files/.
+
+ ## Tests
+ 
+ 
+
+ ## Questions?
+ 
+ Contact jessealdana at Github.
+ 
+ ![Image of user](https://avatars0.githubusercontent.com/u/61436744?v=4)
